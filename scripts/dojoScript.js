f4208baa49a62f5b8ba8118189029d8159acc8cc
marsomate <122260869+marsomate@users.noreply.github.com>
2023-12-21 20:02:15 +0100

    haa

---
+++ scripts/dojoScript.js
@@ -0,0 +1,16 @@
+
+function replaceImage() {
+    var giftImage = document.getElementById('giftImage');
+    giftImage.src = 'images/squash.jpg';
+}
+
+function showPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
+
